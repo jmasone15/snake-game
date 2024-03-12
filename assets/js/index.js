dad063be32bc7390ce6f58b4a83633d349488c5c
Jordan Masone <jordanmasone@jordans-air.lan>
2024-03-12 19:29:53 -0400

    Added wall bump logic.

--- assets/js/index.js
+++ assets/js/index.js
@@ -7,25 +7,51 @@ let snakeCoords = {
     left: 0,
     top: 0
 }
+let snakeHeadWidth = 30;
+
+const snakeBump = (x, y) => {
+    // Wall Bump
+    const horizontalWallBump = x < 0 || x + snakeHeadWidth > window.innerWidth;
+    const verticalWallBump = y < 0 || y + snakeHeadWidth > window.innerHeight;
+    // Snake Bump
+
+    return horizontalWallBump || verticalWallBump
+};
 
 const move = () => {
     switch (direction) {
         case "L":
-            snakeCoords.left -= 10
-            snakeEl.style.left = `${snakeCoords.left}px`
-            break;
+            if (snakeBump(snakeCoords.left - 10, snakeCoords.top)) {
+                return;
+            } else {
+                snakeCoords.left -= 10
+                snakeEl.style.left = `${snakeCoords.left}px`
+                break;
+            }
         case "R":
-            snakeCoords.left += 10
-            snakeEl.style.left = `${snakeCoords.left}px`
-            break;
+            if (snakeBump(snakeCoords.left + 10, snakeCoords.top)) {
+                return;
+            } else {
+                snakeCoords.left += 10
+                snakeEl.style.left = `${snakeCoords.left}px`
+                break;
+            }
         case "U":
-            snakeCoords.top -= 10
-            snakeEl.style.top = `${snakeCoords.top}px`
-            break;
+            if (snakeBump(snakeCoords.left, snakeCoords.top - 10)) {
+                return;
+            } else {
+                snakeCoords.top -= 10
+                snakeEl.style.top = `${snakeCoords.top}px`
+                break;
+            }
         default:
-            snakeCoords.top += 10
-            snakeEl.style.top = `${snakeCoords.top}px`
-            break;
+            if (snakeBump(snakeCoords.left, snakeCoords.top + 10)) {
+                return;
+            } else {
+                snakeCoords.top += 10
+                snakeEl.style.top = `${snakeCoords.top}px`
+                break;
+            }
     }
 }
 
